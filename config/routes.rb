13feca8d445cc20a2af71769f8c6360742c5006d
Bryce Kerley <bkerley@brycekerley.net>
2022-04-09 18:50:14 -0400

    spike dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'dashboard/index', as: 'dashboard'
   root 'sessions#new'
 
   resource :sessions
   resource :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
