0db63cfc41751001a04994c1cbdb23edef345b65
Bryce Kerley <bkerley@brycekerley.net>
2022-04-09 18:40:07 -0400

    working user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    return render action: 'new' unless @user.save
+
+    redirect_to root_path, notice: 'created user'
+  end
+
+  private
+  def user_params
+    params.
+      require(:user).
+      permit(:name,
+             :password,
+             :password_confirmation)
   end
 end
