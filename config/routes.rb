90af9cfc258e6f5d74f528b4bc739ce7b991d330
Bryce Kerley <bkerley@brycekerley.net>
2022-04-09 20:50:19 -0400

    generate credential model

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'credentials/new'
   get 'dashboard/index', as: 'dashboard'
   root 'sessions#new'
 
   resource :sessions
   resource :users
-
+  resources :credentials, only: %i{new create destroy} do
+    post :callback, on: :collection
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ db/migrate/20220410005010_create_credentials.rb
@@ -0,0 +1,14 @@
+class CreateCredentials < ActiveRecord::Migration[7.0]
+  def change
+    create_table :credentials do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.string :name
+      t.string :public_key
+      t.integer :sign_count
+      t.binary :raw_id
+
+      t.timestamps
+    end
+    add_index :credentials, :name, unique: true
+  end
+end
